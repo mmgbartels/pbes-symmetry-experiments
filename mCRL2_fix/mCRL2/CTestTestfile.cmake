8e827e8727129dc078063644cd6a27816e221191
menno <m.m.g.bartels@tue.nl>
2026-03-09 10:33:08 +0100

    retry

---
+++ mCRL2_fix/mCRL2/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/tue20244606/Documents/phd/mcrl2experimentalinstall/mCRL2
+# Build directory: /home/tue20244606/Documents/phd/mcrl2experimentalinstall/mCRL2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("3rd-party/dparser")
+subdirs("3rd-party/tr")
+subdirs("3rd-party/sylvan/src")
+subdirs("libraries")
+subdirs("tools")
+subdirs("cmake/packaging")
